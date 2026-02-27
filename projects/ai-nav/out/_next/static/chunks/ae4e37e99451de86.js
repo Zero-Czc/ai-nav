5a35a314be412b9f72b0a7b9ea4133d30a1aead8
caizhicheng@macmini4 <caizhichengmacmini4@caizhichengmacmini4deMac-mini.local>
2026-02-27 17:37:33 +0800

    清理：无用文件，只保留源码

--- projects/ai-nav/out/_next/static/chunks/ae4e37e99451de86.js
+++
@@ -1,6 +0,0 @@
-(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,31362,t=>{"use strict";var a=t.i(43476);function e(){return(0,a.jsxs)(a.Fragment,{children:[(0,a.jsx)("script",{async:!0,src:"https://www.googletagmanager.com/gtag/js?id=G-NG0YLT2RM0"}),(0,a.jsx)("script",{dangerouslySetInnerHTML:{__html:`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-NG0YLT2RM0');
-        `}})]})}t.s(["default",()=>e])}]);
