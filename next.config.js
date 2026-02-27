2f1e98f130ff25a5441d423a508ae9ace89e2bba
caizhicheng@macmini4 <caizhichengmacmini4@caizhichengmacmini4deMac-mini.local>
2026-02-27 18:38:20 +0800

    优化注释

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 必须开启纯静态导出，这样才能生成 GitHub Pages 认识的纯静态文件
+  // 必须开启纯静态导出，这样才能生成 GitHub Pages 认识的纯静态文件。
   output: 'export',
-  
-  // 禁用图片动态优化（GitHub Pages 不支持服务端动态处理图片）
+  // 禁用图片动态优化（GitHub Pages 不支持服务端动态处理图片）。
   images: {
     unoptimized: true,
   },
